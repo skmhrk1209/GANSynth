72aebf2e8e9a4243226f6e4e0609cc7259c0a1f7
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-18 06:54:23 +0900

    hinge loss

--- models/gan.py
+++ models/gan.py
@@ -45,8 +45,6 @@ class Model(object):
                 dtype=tf.float32,
                 shape=self.next_real_labels.shape
             )
-
-            print("aaaaaaaaaa", self.next_real_images.shape)
             # =========================================================================================
             # placeholders for fake data
             self.fake_latents = tf.placeholder(
@@ -62,6 +60,11 @@ class Model(object):
                 dtype=tf.float32,
                 shape=self.next_fake_labels.shape
             )
+            print("real_images", self.real_images.shape)
+            print("real_labels", self.real_labels.shape)
+            print("fake_latents", self.fake_latents.shape)
+            print("fake_labels", self.fake_labels.shape)
+            print("fake_images", self.fake_images.shape)
             # =========================================================================================
             # logits for real data
             self.real_logits = discriminator(
