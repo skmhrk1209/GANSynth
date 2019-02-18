49b0cbab6e173e8f0bd892f727efec950cd8ef46
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-19 03:19:41 +0900

    update

--- summary.py
+++ summary.py
@@ -12,6 +12,6 @@ def scalar(tensor, name=None, **kwargs):
 def image(tensor, name=None, **kwargs):
 
     name = name or re.sub(":.*", "", tensor.name)
-    if np.argmin(tensor.shape.as_list()[1:]) == 1:
+    if np.argmin(tensor.shape.as_list()[1:]) == 0:
         tensor = tf.transpose(tensor, [0, 2, 3, 1])
     tf.summary.image(name, tensor, **kwargs)
