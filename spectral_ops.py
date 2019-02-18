04630f2313651ea68c3d966bafb6b9069e837331
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-19 07:29:42 +0900

    update

--- spectral_ops.py
+++ spectral_ops.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def diff(x, axis=-1):
+def diff(inputs, axis=-1):
     """Take the finite difference of a tensor along an axis.
     Args:
       x: Input tensor of any dimension.
@@ -12,26 +12,22 @@ def diff(x, axis=-1):
     Raises:
       ValueError: Axis out of range for tensor.
     """
-    shape = x.get_shape()
-    if axis >= len(shape):
-        raise ValueError('Invalid axis index: %d for tensor with only %d axes.' %
-                         (axis, len(shape)))
 
-    begin_back = [0 for unused_s in range(len(shape))]
-    begin_front = [0 for unused_s in range(len(shape))]
+    begin_back = [0] * len(inputs.shape)
+    begin_front = [0] * len(inputs.shape)
     begin_front[axis] = 1
 
-    size = [-1] * len(shape)
-    size[axis] = shape[axis].value - 1
+    size = [-1] * len(inputs.shape)
+    size[axis] = inputs.shape[axis].value - 1
 
-    slice_front = tf.slice(x, begin_front, size)
-    slice_back = tf.slice(x, begin_back, size)
-    d = slice_front - slice_back
+    slice_front = tf.slice(inputs, begin_front, size)
+    slice_back = tf.slice(inputs, begin_back, size)
+    diff = slice_front - slice_back
 
-    return d
+    return diff
 
 
-def unwrap(p, discont=np.pi, axis=-1):
+def unwrap(phases, discont=np.pi, axis=-1):
     """Unwrap a cyclical phase tensor.
     Args:
       p: Phase tensor.
@@ -40,23 +36,23 @@ def unwrap(p, discont=np.pi, axis=-1):
     Returns:
       unwrapped: Unwrapped tensor of same size as input.
     """
-    dd = diff(p, axis=axis)
-    ddmod = tf.mod(dd + np.pi, 2.0 * np.pi) - np.pi
-    idx = tf.logical_and(tf.equal(ddmod, -np.pi), tf.greater(dd, 0))
-    ddmod = tf.where(idx, tf.ones_like(ddmod) * np.pi, ddmod)
-    ph_correct = ddmod - dd
-    idx = tf.less(tf.abs(dd), discont)
-    ddmod = tf.where(idx, tf.zeros_like(ddmod), dd)
-    ph_cumsum = tf.cumsum(ph_correct, axis=axis)
+    diffs = diff(phases, axis=axis)
+    diff_mods = tf.mod(diffs + np.pi, 2.0 * np.pi) - np.pi
+    indices = tf.logical_and(tf.equal(diff_mods, -np.pi), tf.greater(diffs, 0))
+    diff_mods = tf.where(indices, tf.ones_like(diff_mods) * np.pi, diff_mods)
+    phase_corrects = diff_mods - diffs  
+    phase_cumsums = tf.cumsum(phase_corrects, axis=axis)
 
-    shape = p.get_shape().as_list()
+    shape = phases.shape.as_list()
     shape[axis] = 1
-    ph_cumsum = tf.concat([tf.zeros(shape, dtype=p.dtype), ph_cumsum], axis=axis)
-    unwrapped = p + ph_cumsum
+
+    phase_cumsums = tf.concat([tf.zeros(shape), phase_cumsums], axis=axis)
+    unwrapped = phases + phase_cumsums
+
     return unwrapped
 
 
-def instantaneous_frequency(phase_angle, time_axis=-2):
+def instantaneous_frequency(phases, time_axis=-2):
     """Transform a fft tensor from phase angle to instantaneous frequency.
     Unwrap and take the finite difference of the phase. Pad with initial phase to
     keep the tensor the same size.
@@ -66,13 +62,16 @@ def instantaneous_frequency(phase_angle, time_axis=-2):
     Returns:
       dphase: Instantaneous frequency (derivative of phase). Same size as input.
     """
-    phase_unwrapped = unwrap(phase_angle, axis=time_axis)
-    dphase = diff(phase_unwrapped, axis=time_axis)
+    unwrapped = unwrap(phases, axis=time_axis)
+    diffs = diff(unwrapped, axis=time_axis)
 
     # Add an initial phase to dphase
-    size = phase_unwrapped.get_shape().as_list()
+    begin = [0] * len(unwrapped.shape)
+
+    size = [-1] * len(unwrapped.shape)
     size[time_axis] = 1
-    begin = [0 for unused_s in size]
-    phase_slice = tf.slice(phase_unwrapped, begin, size)
-    dphase = tf.concat([phase_slice, dphase], axis=time_axis) / np.pi
-    return dphase
+
+    phase_slice = tf.slice(unwrapped, begin, size)
+    diffs = tf.concat([phase_slice, diffs], axis=time_axis) / np.pi
+    
+    return diffs
