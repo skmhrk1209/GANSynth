89be7a9f5d3fb661992e5ebd7f5b65b3593b0f65
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-19 04:07:43 +0900

    update

--- model.py
+++ model.py
@@ -122,8 +122,8 @@ class GAN(object):
             # utilities
             self.saver = tf.train.Saver()
             # self.fake_images.set_shape(self.real_images.shape)
-            self.real_log_mel_magnitude_spectrograms, self.real_mel_instantaneous_frequencies = tf.unstack(self.real_images, axis=-1)
-            self.fake_log_mel_magnitude_spectrograms, self.fake_mel_instantaneous_frequencies = tf.unstack(self.fake_images, axis=-1)
+            self.real_log_mel_magnitude_spectrograms, self.real_mel_instantaneous_frequencies = tf.unstack(self.real_images, axis=1)
+            self.fake_log_mel_magnitude_spectrograms, self.fake_mel_instantaneous_frequencies = tf.unstack(self.fake_images, axis=1)
             self.summary = tf.summary.merge([
                 summary.image(self.real_log_mel_magnitude_spectrograms, "real_log_mel_magnitude_spectrograms", max_outputs=2),
                 summary.image(self.real_mel_instantaneous_frequencies, "real_mel_instantaneous_frequencies", max_outputs=2),
