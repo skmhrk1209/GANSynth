6bafd4846d86355dbddd6901a315b774197baa55
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-19 04:00:16 +0900

    update

--- summary.py
+++ summary.py
@@ -10,7 +10,11 @@ def scalar(tensor, name=None, **kwargs):
 
 def image(tensor, name=None, data_format="channels_first", **kwargs):
 
+    print(tensor.shape)
+
     name = name or re.sub(":.*", "", tensor.name)
     if data_format == "channels_first":
         tensor = tf.transpose(tensor, [0, 2, 3, 1])
+
+    print(tensor.shape)
     tf.summary.image(name, tensor, **kwargs)
