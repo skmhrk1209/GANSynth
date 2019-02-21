e99b80fbba43843cb83547fe545ad2ce4a98034d
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-21 22:47:17 +0900

    update

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 with open("pitch_counts.json") as f:
     pitch_counts = json.load(f)
+    print(sorted(pitch_counts))
 
 pggan = PGGAN(
     min_resolution=4,
