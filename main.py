af72c0f2aeb6dc321332f7eaab8df9c952cff946
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-02-24 10:11:23 +0900

    change num filters

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ gan = GANSynth(
         generator_learning_rate=8e-4,
         generator_beta1=0.0,
         generator_beta2=0.99,
-        discriminator_learning_rate=8e-4,
+        discriminator_learning_rate=4e-4,  # 8e-4,
         discriminator_beta1=0.0,
         discriminator_beta2=0.99
     ),
