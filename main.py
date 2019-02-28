1bbb0ef0b2ea2fa4699823182f08211cfd7398f0
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-01 05:16:07 +0900

    update

--- main.py
+++ main.py
@@ -82,12 +82,12 @@ with tf.Graph().as_default():
         ),
         hyper_params=Param(
             progress_steps=500000,
-            discriminator_learning_rate=4e-4,
+            discriminator_learning_rate=2e-4,
             discriminator_beta1=0.0,
-            discriminator_beta2=0.99,
-            generator_learning_rate=8e-4,
+            discriminator_beta2=0.9,
+            generator_learning_rate=2e-4,
             generator_beta1=0.0,
-            generator_beta2=0.99
+            generator_beta2=0.9
         ),
         name=args.model_dir
     )
