069d8df79c5eec5f52097bf96f1b816e43e2827e
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-02 03:38:40 +0900

    applied conditional batch norm

--- network.py
+++ network.py
@@ -92,69 +92,54 @@ class PGGAN(object):
                     inputs = tf.nn.tanh(inputs)
                 return inputs
 
-        out_depth = scale(progress, 0.0, 1.0, self.min_depth, self.max_depth)
-
         def lerp(a, b, t): return t * a + (1 - t) * b
 
         def grow(feature_maps, depth):
-            ''' depthに対応する層によって特徴マップを画像として生成
-            Args:
-                feature_maps: 1つ浅い層から受け取った特徴マップ
-                depth: 現在見ている層の深さ（解像度が上がる方向に深くなると定義）
-            Returns:
-                images: 最終的な生成画像
-            '''
 
-            # 現在より深い層によって生成された画像（ここで再帰）
             def high_resolution_images():
-                return grow(conv_block(feature_maps, depth), depth + 1)
+                return grow(residual_block(feature_maps, depth), depth + 1)
 
-            # 現在の層の解像度で生成した画像を最終解像度までupscaleする
             def middle_resolution_images():
                 return upscale2d(
-                    inputs=color_block(conv_block(feature_maps, depth), depth),
+                    inputs=color_block(residual_block(feature_maps, depth), depth),
                     factors=resolution(self.max_depth) // resolution(depth)
                 )
 
-            # 1つ浅い層の解像度で生成した画像を最終解像度までupscaleする
             def low_resolution_images():
                 return upscale2d(
                     inputs=color_block(feature_maps, depth - 1),
                     factors=resolution(self.max_depth) // resolution(depth - 1)
                 )
 
-            # 最も浅い層はlow_resolution_imagesは選択肢にない
             if depth == self.min_depth:
                 images = tf.cond(
-                    pred=tf.greater(out_depth, depth),
+                    pred=tf.greater(growing_depth, depth),
                     true_fn=high_resolution_images,
                     false_fn=middle_resolution_images
                 )
-            # 最も深い層はhigh_resolution_imagesは選択肢にない
             elif depth == self.max_depth:
                 images = tf.cond(
-                    pred=tf.greater(out_depth, depth),
+                    pred=tf.greater(growing_depth, depth),
                     true_fn=middle_resolution_images,
                     false_fn=lambda: lerp(
                         a=low_resolution_images(),
                         b=middle_resolution_images(),
-                        t=depth - out_depth
+                        t=depth - growing_depth
                     )
                 )
-            # それ以外は以下のいずれかを出力する
-            # 1. high_resolution_images
-            # 2. low_resolution_imagesとmiddle_resolution_imagesの線形補間
             else:
                 images = tf.cond(
-                    pred=tf.greater(out_depth, depth),
+                    pred=tf.greater(growing_depth, depth),
                     true_fn=high_resolution_images,
                     false_fn=lambda: lerp(
                         a=low_resolution_images(),
                         b=middle_resolution_images(),
-                        t=depth - out_depth
+                        t=depth - growing_depth
                     )
                 )
             return images
+
+        growing_depth = scale(progress, 0.0, 1.0, self.min_depth, self.max_depth)
 
         with tf.variable_scope(name, reuse=reuse):
             return grow(tf.concat([latents, labels], axis=-1), self.min_depth)
@@ -254,69 +239,54 @@ class PGGAN(object):
                     inputs = tf.nn.leaky_relu(inputs)
                 return inputs
 
-        in_depth = scale(progress, 0.0, 1.0, self.min_depth, self.max_depth)
-
         def lerp(a, b, t): return t * a + (1 - t) * b
 
         def grow(images, depth):
-            ''' depthに対応する層によって画像を特徴マップとして取り込む
-            Args:
-                images: 入力画像（depthに関わらず一定）
-                depth: 現在見ている層の深さ（解像度が上がる方向に深くなると定義）
-            Returns:
-                feature_maps: 1つ浅い層に渡す特徴マップ
-            '''
 
-            # 現在より深い層によって取り込まれた特徴マップ（ここで再帰）
             def high_resolution_feature_maps():
-                return conv_block(grow(images, depth + 1), depth)
+                return residual_block(grow(images, depth + 1), depth)
 
-            # 現在の層の解像度までdownscaleした後，特徴マップとして取り込む
             def middle_resolution_feature_maps():
-                return conv_block(color_block(downscale2d(
+                return residual_block(color_block(downscale2d(
                     inputs=images,
                     factors=resolution(self.max_depth) // resolution(depth)
                 ), depth), depth)
 
-            # 1つ浅い層の解像度までdownscaleした後，特徴マップとして取り込む
             def low_resolution_feature_maps():
                 return color_block(downscale2d(
                     inputs=images,
                     factors=resolution(self.max_depth) // resolution(depth - 1)
                 ), depth - 1)
 
-            # 最も浅い層はlow_resolution_feature_mapsは選択肢にない
             if depth == self.min_depth:
                 feature_maps = tf.cond(
-                    pred=tf.greater(in_depth, depth),
+                    pred=tf.greater(growing_depth, depth),
                     true_fn=high_resolution_feature_maps,
                     false_fn=middle_resolution_feature_maps
                 )
-            # 最も深い層はhigh_resolution_feature_mapsは選択肢にない
             elif depth == self.max_depth:
                 feature_maps = tf.cond(
-                    pred=tf.greater(in_depth, depth),
+                    pred=tf.greater(growing_depth, depth),
                     true_fn=middle_resolution_feature_maps,
                     false_fn=lambda: lerp(
                         a=low_resolution_feature_maps(),
                         b=middle_resolution_feature_maps(),
-                        t=depth - in_depth
+                        t=depth - growing_depth
                     )
                 )
-            # それ以外は以下のいずれかを出力する
-            # 1. high_resolution_feature_maps
-            # 2. low_resolution_feature_mapsとmiddle_resolution_feature_mapsの線形補間
             else:
                 feature_maps = tf.cond(
-                    pred=tf.greater(in_depth, depth),
+                    pred=tf.greater(growing_depth, depth),
                     true_fn=high_resolution_feature_maps,
                     false_fn=lambda: lerp(
                         a=low_resolution_feature_maps(),
                         b=middle_resolution_feature_maps(),
-                        t=depth - in_depth
+                        t=depth - growing_depth
                     )
                 )
             return feature_maps
+
+        growing_depth = scale(progress, 0.0, 1.0, self.min_depth, self.max_depth)
 
         with tf.variable_scope(name, reuse=reuse):
             return grow(images, self.min_depth)
