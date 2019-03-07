3f42197070925407163a51186db49f57915d5329
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-08 04:51:43 +0900

    training pipeline changed

--- main.py
+++ main.py
@@ -12,7 +12,6 @@
 #=================================================================================================#
 
 import tensorflow as tf
-import numpy as np
 import argparse
 import functools
 import pickle
--- model.py
+++ model.py
@@ -1,8 +1,4 @@
 import tensorflow as tf
-import numpy as np
-import itertools
-import functools
-import os
 
 
 class GANSynth(object):
