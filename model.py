320170a791a8c49171eb861765c12693cbedef02
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-07 23:33:38 +0900

    update

--- model.py
+++ model.py
@@ -109,8 +109,10 @@ class GANSynth(object):
         generator_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope="generator")
         discriminator_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope="discriminator")
         # -----------------------------------------------------------------------------------------
-        self.generator_train_op = tf.group([generator_train_op, generator_update_ops])
-        self.discriminator_train_op = tf.group([discriminator_train_op, discriminator_update_ops])
+        # self.generator_train_op = tf.group([generator_train_op, generator_update_ops])
+        # self.discriminator_train_op = tf.group([discriminator_train_op, discriminator_update_ops])
+        self.generator_train_op = generator_train_op
+        self.discriminator_train_op = discriminator_train_op
         # =========================================================================================
         # scaffold
         self.scaffold = tf.train.Scaffold(
