4668ff604d87fa8f0afbbe935dc6ed1fd1f42319
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-10 20:32:24 +0900

    update

--- make_tfrecord.py
+++ make_tfrecord.py
@@ -10,8 +10,8 @@ def main(in_dir1, in_dir2, in_file, out_file):
             ground_truth = json.load(file)
         for key, value in ground_truth.items():
             writer.write(record=tf.train.Example(features=tf.train.Features(feature=dict(
-                path1=tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(in_dir1 / Path(key).with_suffix(".jpg"))])),
-                path2=tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(in_dir2 / Path(key).with_suffix(".jpg"))])),
+                path1=tf.train.Feature(bytes_list=tf.train.BytesList(value=[str(in_dir1 / Path(key).with_suffix(".jpg")).encode()])),
+                path2=tf.train.Feature(bytes_list=tf.train.BytesList(value=[str(in_dir2 / Path(key).with_suffix(".jpg")).encode()])),
                 pitch=tf.train.Feature(int64_list=tf.train.Int64List(value=[value["pitch"]])),
                 source=tf.train.Feature(int64_list=tf.train.Int64List(value=[value["instrument_source"]]))
             ))).SerializeToString())
