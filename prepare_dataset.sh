8a8e8167a8f97501d8486fd5229cbf54a7f58e3d
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-10 10:09:45 +0900

    update

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -8,13 +8,13 @@
 
 # python preprocess.py nsynth-train/audio nsynth-train/log_mel_magnitude_spectrograms nsynth-train/mel_instantaneous_frequencies
 # python preprocess.py nsynth-valid/audio nsynth-valid/log_mel_magnitude_spectrograms nsynth-valid/mel_instantaneous_frequencies
-python preprocess.py        nsynth-test/audio \
+# python preprocess.py        nsynth-test/audio \
                             nsynth-test/log_mel_magnitude_spectrograms \
                             nsynth-test/mel_instantaneous_frequencies
 
 #python make_tfrecord.py nsynth/train/gt.txt nsynth_train.tfrecord
 #python make_tfrecord.py nsynth/valid/gt.txt nsynth_valid.tfrecord
-python make_tfrecord.py     nsynth-test/log_mel_magnitude_spectrograms \
+# python make_tfrecord.py     nsynth-test/log_mel_magnitude_spectrograms \
                             nsynth-test/mel_instantaneous_frequencies \
                             nsynth-test/examples.json \
                             nsynth-test/examples.tfrecord
