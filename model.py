2ae502b34b493c0f25b6c39b3920393b187b74b9
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-14 23:00:24 +0900

    update

--- model.py
+++ model.py
@@ -236,7 +236,7 @@ class GANSynth(object):
                 fake_logits=[]
             )
 
-            while True:
+            for i in range(10):
                 try:
                     real_features, real_logits, fake_features, fake_logits = session.run([
                         self.tensors.real_features,
