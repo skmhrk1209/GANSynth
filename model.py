93553fc0cd7a30538790370ee8e6c0e40e4434ff
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-14 22:43:28 +0900

    update

--- model.py
+++ model.py
@@ -270,7 +270,7 @@ class GANSynth(object):
                 return np.sum((real_mean - fake_mean) ** 2) + np.trace(real_cov + fake_cov - 2 * sp.linalg.sqrtm(np.dot(real_cov, fake_cov)))
 
             tf.logging.info("inception_score: {}, frechet_classifier_distance: {}".format(
-                inception_score(predictions.fake_logits),
+                inception_score(predictions.real_logits),
                 frechet_classifier_distance(predictions.real_features, predictions.fake_features)
             ))
 
