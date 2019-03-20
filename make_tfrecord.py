cdb1b35a752e067a688cdde0e3abb57c9c8b43a3
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-03-20 20:37:58 +0900

    fix fake_input_fn bug

--- make_tfrecord.py
+++ make_tfrecord.py
@@ -47,58 +47,58 @@ def main(in_file, out_file):
 if __name__ == "__main__":
 
     with open("nsynth-train/examples.json") as file:
-        train_ground_truth = json.load(file)
+        nsynth_train_examples = json.load(file)
 
     with open("nsynth-valid/examples.json") as file:
-        valid_ground_truth = json.load(file)
+        nsynth_valid_examples = json.load(file)
 
     with open("nsynth-test/examples.json") as file:
-        test_ground_truth = json.load(file)
+        nsynth_test_examples = json.load(file)
 
-    for key in train_ground_truth:
-        train_ground_truth[key].update(dict(
+    for key in nsynth_train_examples:
+        nsynth_train_examples[key].update(dict(
             path_to_waveform="nsynth-train/audio/{}.wav".format(key),
             path_to_magnitude_spectrogram="nsynth-train/magnitude_spectrograms/{}.jpg".format(key),
             path_to_instantaneous_frequency="nsynth-train/instantaneous_frequencies/{}.jpg".format(key)
         ))
 
-    for key in valid_ground_truth:
-        valid_ground_truth[key].update(dict(
+    for key in nsynth_valid_examples:
+        nsynth_valid_examples[key].update(dict(
             path_to_waveform="nsynth-valid/audio/{}.wav".format(key),
             path_to_magnitude_spectrogram="nsynth-valid/magnitude_spectrograms/{}.jpg".format(key),
             path_to_instantaneous_frequency="nsynth-valid/instantaneous_frequencies/{}.jpg".format(key)
         ))
 
-    for key in test_ground_truth:
-        test_ground_truth[key].update(dict(
+    for key in nsynth_test_examples:
+        nsynth_test_examples[key].update(dict(
             path_to_waveform="nsynth-test/audio/{}.wav".format(key),
             path_to_magnitude_spectrogram="nsynth-test/magnitude_spectrograms/{}.jpg".format(key),
             path_to_instantaneous_frequency="nsynth-test/instantaneous_frequencies/{}.jpg".format(key)
         ))
 
-    mixed_ground_truth = list(dict(
-        **train_ground_truth,
-        **valid_ground_truth,
-        **test_ground_truth
+    nsynth_examples = list(dict(
+        **nsynth_train_examples,
+        **nsynth_valid_examples,
+        **nsynth_test_examples
     ).items())
 
-    random.shuffle(mixed_ground_truth)
+    random.shuffle(nsynth_examples)
 
-    train_ground_truth, valid_ground_truth, test_ground_truth = [
-        mixed_ground_truth[begin:end] for begin, end in zip(
-            [None, int(len(mixed_ground_truth) * 0.8), int(len(mixed_ground_truth) * 0.9)],
-            [int(len(mixed_ground_truth) * 0.8), int(len(mixed_ground_truth) * 0.9), None]
+    nsynth_train_examples, nsynth_valid_examples, nsynth_test_examples = [
+        nsynth_examples[begin:end] for begin, end in zip(
+            [None, int(len(nsynth_examples) * 0.8), int(len(nsynth_examples) * 0.9)],
+            [int(len(nsynth_examples) * 0.8), int(len(nsynth_examples) * 0.9), None]
         )
     ]
 
     with open("nsynth_train_examples.json", "w") as file:
-        json.dump(dict(train_ground_truth), file, indent=4)
+        json.dump(dict(nsynth_train_examples), file, indent=4)
 
     with open("nsynth_valid_examples.json", "w") as file:
-        json.dump(dict(valid_ground_truth), file, indent=4)
+        json.dump(dict(nsynth_valid_examples), file, indent=4)
 
     with open("nsynth_test_examples.json", "w") as file:
-        json.dump(dict(test_ground_truth), file, indent=4)
+        json.dump(dict(nsynth_test_examples), file, indent=4)
 
     main("nsynth_train_examples.json", "nsynth_train_examples.tfrecord")
     main("nsynth_valid_examples.json", "nsynth_valid_examples.tfrecord")
