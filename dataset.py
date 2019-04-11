bfbdd44fe40be67a5e7dfdad3477824b2ec3e47c
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-04-11 19:13:17 +0900

    update

--- dataset.py
+++ dataset.py
@@ -9,7 +9,8 @@ from utils import Struct
 from tensorflow.contrib.framework.python.ops import audio_ops
 
 
-def nsynth_input_fn(filenames, batch_size, num_epochs, shuffle, pitches, sources):
+def nsynth_input_fn(filenames, batch_size, num_epochs, shuffle,
+                    buffer_size=None, pitches=None, sources=None):
 
     index_table = tf.contrib.lookup.index_table_from_tensor(sorted(pitches), dtype=tf.int32)
 
@@ -41,39 +42,26 @@ def nsynth_input_fn(filenames, batch_size, num_epochs, shuffle, pitches, sources
 
         return waveform, label, pitch, source
 
-    dataset = tf.data.TFRecordDataset(filenames=filenames)
+    dataset = tf.data.TFRecordDataset(filenames)
     if shuffle:
-        dataset = dataset.shuffle(
-            buffer_size=sum([
-                len(list(tf.io.tf_record_iterator(filename)))
-                for filename in filenames
-            ]),
-            reshuffle_each_iteration=True
-        )
-    dataset = dataset.repeat(count=num_epochs)
-    dataset = dataset.map(
-        map_func=parse_example,
-        num_parallel_calls=os.cpu_count()
-    )
+        dataset = dataset.shuffle(buffer_size or sum([
+            len(list(tf.io.tf_record_iterator(filename)))
+            for filename in filenames
+        ]), reshuffle_each_iteration=True)
+    dataset = dataset.repeat(num_epochs)
+    dataset = dataset.map(parse_example, num_parallel_calls=os.cpu_count())
     # filter just acoustic instruments and just pitches 24-84 (as in the paper)
-    dataset = dataset.filter(
-        predicate=lambda waveform, label, pitch, source: tf.logical_and(
-            x=tf.reduce_any(tf.equal(sources, source)),
-            y=tf.logical_and(
-                x=tf.greater_equal(pitch, min(pitches)),
-                y=tf.less_equal(pitch, max(pitches))
-            )
-        )
-    )
-    dataset = dataset.map(
-        map_func=lambda waveform, label, pitch, source: (waveform, label),
-        num_parallel_calls=os.cpu_count()
-    )
-    dataset = dataset.batch(
-        batch_size=batch_size,
-        drop_remainder=True
-    )
-    dataset = dataset.prefetch(buffer_size=1)
+    dataset = dataset.filter(lambda waveform, label, pitch, source: functools.reduce(
+        tf.logical_and,
+        filter(None, [
+            tf.greater_equal(pitch, min(pitches)) if pitches else pitches,
+            tf.less_equal(pitch, max(pitches)) if pitches else pitches,
+            tf.reduce_any(tf.equal(sources, source)) if sources else sources,
+        ])
+    ))
+    dataset = dataset.map(lambda waveform, label, pitch, source: (waveform, label), num_parallel_calls=os.cpu_count())
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    dataset = dataset.prefetch(1)
 
     iterator = dataset.make_initializable_iterator()
 
