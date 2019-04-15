378af2723e6e90a6313f53ddca1698c76dd35187
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-04-15 10:39:00 +0900

    update

--- a.py
+++ a.py
@@ -6,7 +6,7 @@ import time
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-def batch_normalization(inputs, training, momentum=0.99, epsilon=1.0e-12):
+def _batch_normalization(inputs, training, momentum=0.99, epsilon=1.0e-12):
     return tf.layers.batch_normalization(
         inputs=inputs,
         axis=1,
@@ -17,7 +17,7 @@ def batch_normalization(inputs, training, momentum=0.99, epsilon=1.0e-12):
     )
 
 
-def _batch_normalization(inputs, training, momentum=0.99, epsilon=1.0e-12):
+def batch_normalization(inputs, training, momentum=0.99, epsilon=1.0e-12):
     def assign_moving_average(variable, value):
         return tf.assign_sub(variable, (variable - value) * (1.0 - momentum))
     training = tf.convert_to_tensor(training)
@@ -39,26 +39,25 @@ def _batch_normalization(inputs, training, momentum=0.99, epsilon=1.0e-12):
         axes=[0] + list(range(2, len(shape))),
         keep_dims=True
     )
+    mean = tf.cond(training, lambda: mean, lambda: moving_mean)
+    variance = tf.cond(training, lambda: variance, lambda: moving_variance)
+    stddev = tf.sqrt(variance + epsilon)
+    inputs = (inputs - mean) / stddev
+    beta = tf.get_variable(
+        name="beta",
+        shape=[1, shape[1]] + [1] * len(shape[2:]),
+        initializer=tf.initializers.zeros()
+    )
+    gamma = tf.get_variable(
+        name="gamma",
+        shape=[1, shape[1]] + [1] * len(shape[2:]),
+        initializer=tf.initializers.ones()
+    )
+    inputs = inputs * gamma + beta
     moving_mean = tf.cond(training, lambda: assign_moving_average(moving_mean, mean), lambda: moving_mean)
     moving_variance = tf.cond(training, lambda: assign_moving_average(moving_variance, variance), lambda: moving_variance)
     tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, moving_mean)
     tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, moving_variance)
-    with tf.control_dependencies([moving_mean, moving_variance]):
-        mean = tf.cond(training, lambda: mean, lambda: moving_mean)
-        variance = tf.cond(training, lambda: variance, lambda: moving_variance)
-        stddev = tf.sqrt(variance + epsilon)
-        inputs = (inputs - mean) / stddev
-        beta = tf.get_variable(
-            name="beta",
-            shape=[1, shape[1]] + [1] * len(shape[2:]),
-            initializer=tf.initializers.zeros()
-        )
-        gamma = tf.get_variable(
-            name="gamma",
-            shape=[1, shape[1]] + [1] * len(shape[2:]),
-            initializer=tf.initializers.ones()
-        )
-        inputs = inputs * gamma + beta
     return inputs
 
 
