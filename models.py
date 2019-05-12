c59bda769a1fec72a2c1b5b3136ab8235aff21d5
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-05-12 16:34:50 +0900

    update

--- models.py
+++ models.py
@@ -209,11 +209,12 @@ class GANSynth(object):
 
             real_features, real_logits, fake_features, fake_logits = map(np.concatenate, zip(*generator()))
 
-            print(
-                f"frechet_inception_distance: {metrics.frechet_inception_distance(real_features, fake_features)},"
-                f"inception_score: {metrics.inception_score(real_logits), metrics.inception_score(fake_logits)},"
-                f"num_different_bins: {metrics.num_different_bins(real_features, fake_features)}"
-            )
+            print("----------------------------------------------------------------")
+            print("evaluation result")
+            print(f"frechet_inception_distance: {metrics.frechet_inception_distance(real_features, fake_features)}")
+            print(f"inception_score: {metrics.inception_score(real_logits), metrics.inception_score(fake_logits)}")
+            print(f"num_different_bins: {metrics.num_different_bins(real_features, fake_features)}")
+            print("----------------------------------------------------------------")
 
 
 class PitchClassifier(object):
@@ -365,6 +366,8 @@ class PitchClassifier(object):
 
             while not session.should_stop():
                 accuracy = session.run(self.update_op)
-                print(accuracy)
 
+            print("----------------------------------------------------------------")
+            print("evaluation result")
             print(f"accuracy: {accuracy}")
+            print("----------------------------------------------------------------")
