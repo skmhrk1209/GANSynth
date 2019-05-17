91b2e12e2796b07270e5d95778737db91bb650a2
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-05-17 19:29:44 +0900

    update

--- models.py
+++ models.py
@@ -237,7 +237,7 @@ class GANSynth(object):
         ) as session:
 
             def generator():
-                print("b")
+                print(session.should_stop())
                 while not session.should_stop():
                     print("a")
                     try:
