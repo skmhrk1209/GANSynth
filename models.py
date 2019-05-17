412c538d6038ac4840f71a6a7c89ba6a17495aed
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2019-05-18 01:31:21 +0900

    update

--- models.py
+++ models.py
@@ -222,20 +222,17 @@ class GANSynth(object):
             def generator():
                 while not session.should_stop():
                     try:
-                        yield session.run([self.real_features, self.fake_features])
+                        yield session.run([
+                            self.real_features,
+                            self.fake_features,
+                            self.real_magnitude_spectrograms,
+                            self.fake_magnitude_spectrograms
+                        ])
                     except tf.errors.OutOfRangeError:
                         break
 
-            real_features, fake_features = map(np.concatenate, zip(*generator()))
+            real_features, fake_features, real_magnitude_spectrograms, fake_magnitude_spectrograms = map(np.concatenate, zip(*generator()))
 
-            def generator():
-                while not session.should_stop():
-                    try:
-                        yield session.run([self.real_magnitude_spectrograms, self.fake_magnitude_spectrograms])
-                    except tf.errors.OutOfRangeError:
-                        break
-
-            real_magnitude_spectrograms, fake_magnitude_spectrograms = map(np.concatenate, zip(*generator()))
             real_magnitude_spectrograms = np.reshape(real_magnitude_spectrograms, [real_magnitude_spectrograms.shape[0], -1])
             fake_magnitude_spectrograms = np.reshape(real_magnitude_spectrograms, [fake_magnitude_spectrograms.shape[0], -1])
 
